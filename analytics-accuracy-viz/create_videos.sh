7655ec65ced02a7c86c7fa71ef73e4741b517495
Ben Civjan <bencivjan@gmail.com>
2024-07-10 12:09:14 -0700

    New iou calculation

--- analytics-accuracy-viz/create_videos.sh
+++ analytics-accuracy-viz/create_videos.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Source video file from https://www.kaggle.com/datasets/smeschke/pedestrian-dataset/data
-input_video="crosswalk.avi"
+# input_video="videos/crosswalk.avi"
+input_video="videos/ny_driving.mov"
 
 # Array of bitrate and frame rate combinations to encode
-bitrates=("250K" "500k" "750K" "1m")
+# bitrates=("250K" "500k" "750K" "1m")
+bitrates=("1m" "3m" "5m")
 framerates=("10" "15" "30")
 
 # Loop through each combination of bitrate and frame rate
 for bitrate in "${bitrates[@]}"; do
     for framerate in "${framerates[@]}"; do
         # Output file name based on bitrate and framerate
-        output_file="crosswalk_${bitrate}_${framerate}.mp4"
+        output_file="nydriving_${bitrate}_${framerate}.mp4"
 
         # FFmpeg command to transcode
-        ffmpeg -i "$input_video" -vf "fps=$framerate" -b:v "$bitrate" -c:v libx264 "$output_file"
+        ffmpeg -i "$input_video" -s 1920x1080 -vf "fps=$framerate" -b:v "$bitrate" -c:v libx264 "$output_file"
     done
 done
