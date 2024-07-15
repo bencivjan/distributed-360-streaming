7985ae408ffc5f3f6b30cd480302df8ed4fbe480
Ben Civjan <bencivjan@gmail.com>
2024-07-15 10:48:32 -0500

    Update network script

--- measure_network.sh
+++ measure_network.sh
@@ -15,33 +15,18 @@ if [[ "$role" == "c" ]]; then
 
     # Run the iperf3 server and write the output to the specified file
     echo "Starting iperf3 client..."
-    iperf3 -c $server_ip -i 1 -V -u -b 10M -t 30 2>&1 | tee $output_file &
-    iperf3_pid=$!
-
-    # Output the PID of the iperf3 server
     echo "Output is being written to $output_file"
 
-    # Trap to ensure the iperf3 server is killed when the script exits
-    trap "kill $iperf3_pid" EXIT
-
-    # Wait for the iperf3 server process to complete
-    wait $iperf3_pid
+    iperf3 -c $server_ip -i 1 -V -t 30 2>&1 | tee -a $output_file
+    iperf3 -c $server_ip -i 1 -V -u -b 10M -t 30 2>&1 | tee -a $output_file
 
 elif [[ "$role" == "s" ]]; then
 
     # Run the iperf3 server and write the output to the specified file
     echo "Starting iperf3 server..."
-    iperf3 -s -V -1 2>&1 | tee $output_file &
-    iperf3_pid=$!
+    iperf3 -s -V 2>&1 | tee $output_file
 
-    # Output the PID of the iperf3 server
     echo "Output is being written to $output_file"
-
-    # Trap to ensure the iperf3 server is killed when the script exits
-    trap "kill $iperf3_pid" EXIT
-
-    # Wait for the iperf3 server process to complete
-    wait $iperf3_pid
 
 else
     echo "Input either 'c' or 's' as first argument"
